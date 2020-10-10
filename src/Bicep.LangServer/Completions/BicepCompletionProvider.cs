824f98090b2884b76f871d4076aba451518d987c
Marcin Jastrzebski <majastrz@microsoft.com>
2020-10-10 02:07:14 -0700

    better looking completions

--- src/Bicep.LangServer/Completions/BicepCompletionProvider.cs
+++ src/Bicep.LangServer/Completions/BicepCompletionProvider.cs
@@ -4,7 +4,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using Bicep.Core;
+using Bicep.Core.Parser;
 using Bicep.Core.SemanticModel;
 using Bicep.Core.TypeSystem;
 using Bicep.LanguageServer.Snippets;
@@ -14,6 +16,8 @@ namespace Bicep.LanguageServer.Completions
 {
     public class BicepCompletionProvider : ICompletionProvider
     {
+        private const string MarkdownNewLine = "  \n";
+
         public IEnumerable<CompletionItem> GetFilteredCompletions(SemanticModel model, BicepCompletionContext context)
         {
             return GetDeclarationCompletions(context)
@@ -156,7 +160,7 @@ namespace Bicep.LanguageServer.Completions
                 return Enumerable.Empty<CompletionItem>();
             }
 
-            return GetProperties(declaredType).Select(property => CreateKeywordCompletion(property.Name, $"{property.Name} ({property.TypeReference.Type.Name})"));
+            return GetProperties(declaredType).Select(CreatePropertyCompletion);
         }
 
         private static IEnumerable<TypeProperty> GetProperties(TypeSymbol type)
@@ -170,6 +174,56 @@ namespace Bicep.LanguageServer.Completions
                 default:
                     return Enumerable.Empty<TypeProperty>();
             }
+        }
+
+        private static CompletionItem CreatePropertyCompletion(TypeProperty property)
+        {
+            return new CompletionItem
+            {
+                Kind = CompletionItemKind.Property,
+                Label = property.Name,
+                InsertTextFormat = InsertTextFormat.PlainText,
+                // property names containg spaces need to be escaped
+                InsertText = Lexer.IsValidIdentifier(property.Name) ? property.Name : StringUtils.EscapeBicepString(property.Name),
+                CommitCharacters = new Container<string>(" ", ":"),
+                Detail = FormatPropertyDetail(property),
+                Documentation = new StringOrMarkupContent(new MarkupContent
+                {
+                    Kind = MarkupKind.Markdown,
+                    Value = FormatPropertyDocumentation(property)
+                })
+            };
+        }
+
+        private static string FormatPropertyDetail(TypeProperty property) =>
+            property.Flags.HasFlag(TypePropertyFlags.Required)
+                ? $"{property.Name} (Required)"
+                : property.Name;
+
+        private static string FormatPropertyDocumentation(TypeProperty property)
+        {
+            var buffer = new StringBuilder();
+
+            buffer.Append($"Type: `{property.TypeReference.Type}`{MarkdownNewLine}");
+
+            if (property.Flags.HasFlag(TypePropertyFlags.ReadOnly))
+            {
+                // this case will be used for dot property access completions
+                // this flag is not possible in property name completions
+                buffer.Append($"Read-only property{MarkdownNewLine}");
+            }
+
+            if (property.Flags.HasFlag(TypePropertyFlags.WriteOnly))
+            {
+                buffer.Append($"Write-only property{MarkdownNewLine}");
+            }
+
+            if (property.Flags.HasFlag(TypePropertyFlags.Constant))
+            {
+                buffer.Append($"Requires a compile-time constant value.{MarkdownNewLine}");
+            }
+
+            return buffer.ToString();
         }
 
         private static CompletionItem CreateKeywordCompletion(string keyword, string detail) =>
